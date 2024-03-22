c5e9120f08b658a1ee1a0f4ce17c724dd0360135
Akshay Narayanan B <90465549+NarayanAkshay11@users.noreply.github.com>
2024-03-22 11:07:52 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// You can add any additional functionality or interactivity here
+const aboutBtn = document.querySelector('.about-btn');
+
+aboutBtn.addEventListener('click', () => {
+    // Navigate to the "About Us" page or display additional information
+    window.location.href = 'about.html';
+});
